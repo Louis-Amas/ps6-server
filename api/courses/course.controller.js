d4eb7ca6d5478a89570a853cbb6a597c70ddac86
Louis-Amas <amaslouis@gmail.com>
2019-03-26 14:59:23 +0100

    Added formating #5

--- api/courses/course.controller.js
+++ api/courses/course.controller.js
@@ -1,13 +1,23 @@
 const CourseModel = require('../../models/Course');
 
+const formatCourses = (courses) => {
+  console.log(courses);
+  return courses.reduce((prev, curr) => {
+    const cop = curr._doc;
+    delete cop.__v;
+    prev.push(cop);
+    return prev;
+  }, []);
+};
+
 exports.getAllCourseFromUniversityIdAndMajor = (req, res) => {
-  console.log(req.params);
   if (!req.params.univId || ! req.params.major)
     return res.status(400).json("Bad request");
   CourseModel.getByUnivIdAndMajor(req.params.univId, req.params.major)
     .then((courses) => {
-      return res.status(200).json(courses);
+      return res.status(200).json(formatCourses(courses));
     }).catch(err => {
-      return res.status(400).json(err);
+      return res.status(404).send();
   });
 };
+
